ac1fe383e887764164def58a1699892edf06fc94
Katerina <alisyatina@yandex.ru>
2021-03-11 22:14:14 +0300

    11.03.2121

--- js/main.js
+++ js/main.js
@@ -85,11 +85,18 @@ let renderFoto = (foto) => {
   pictureImg.src = foto.url;
   pictureComments.textContent = foto.comments.length;
   pictureLikes.textContent = foto.likes;
-  // ----------------------------------------- Добавление Фото в  просмотр при нажатии //
-  pictureImg.onclick = function () {
+  // ----------------------------------------- Добавление Фото в полноэкранный просмотр при нажатии //
+  pictureImg.addEventListener("click", function () {
     bigPictureImg.src = foto.url;
     bigPicture.classList.remove("hidden");
-  };
+  });
+
+  fotoElement.addEventListener("keydown", function (evt) {
+    if (evt.key === "Enter") {
+      bigPictureImg.src = foto.url;
+      bigPicture.classList.remove("hidden");
+    }
+  });
 
   return fotoElement;
 };
@@ -101,15 +108,6 @@ for (let i = 0; i < fotos.length; i++) {
 usersFotos.appendChild(fragment);
 
 // ========================================================================Второе задание==========================================================================//
-
-// picture.forEach((item) => {
-//   // Функция перебора  //
-//   item.onclick = function () {
-//     // Функция клика по маленькой фотографии //
-//     bigPictureImg.src = foto.url;
-//     console.log(item);
-//   };
-// });
 
 likesCount.textContent = fotos[0].likes;
 commentsCount.textContent = getRandomIntInclusive(1, 50);
@@ -138,42 +136,158 @@ document.addEventListener("keydown", function (evt) {
 let uploadFile = document.querySelector("#upload-file"); // Кнопка загрузки фотографий //
 let imgUploadOverlay = document.querySelector(".img-upload__overlay"); // Окно редактирования //
 let uploadCancel = document.querySelector("#upload-cancel"); // Кнопка закрытия окна редактирования //
-uploadFile.onchange = () => {
+uploadFile.addEventListener("change", function () {
   imgUploadOverlay.classList.remove("hidden"); // Показываем окно редактирования //
   body.classList.add("modal-open"); // Модальное окно открыто на все BODY //
-};
+});
+
+// ======================================= работа с окном редактирования ============================================ //
+
+// ========================================== Работа с масштабом ====================================================== //
+let scaleControlSmaller = document.querySelector(".scale__control--smaller"); // Кнопка уменьшения изображения //
+let scaleControlBigger = document.querySelector(".scale__control--bigger"); // Кнопка увеличения изображения //
+let scaleControlValue = document.querySelector(".scale__control--value"); //  Поле, значение которого должно меняться //
+let imgUploadPreview = document.querySelector(".img-upload__preview"); // картинка для редактирования //
+let imgUploadEffectLevel = document.querySelector(".img-upload__effect-level"); // Слайдер //
+let effectLevelPin = document.querySelector(".effect-level__pin"); // Ползунок в слайдере //
+// let effectsPreviewNone = document.querySelector("effects__preview--none");
+// effectLevelPin.addEventListener("mouseup", function () {});
+imgUploadEffectLevel.classList.add("hidden");
+
+scaleControlValue.value = "100";
+scaleControlSmaller.addEventListener("click", function () {
+  let number;
+  let scaleValue = Number(scaleControlValue.value);
+  if (scaleValue <= 100) {
+    number = scaleValue - 25;
+    scaleControlBigger.disabled = false;
+  }
+  if (scaleValue <= 50) {
+    scaleControlSmaller.disabled = true;
+  }
+  scaleControlValue.value = number;
+  imgUploadPreview.style.transform = `scale(${number / 100})`;
+});
+scaleControlBigger.addEventListener("click", function () {
+  let number;
+  let scaleValue = Number(scaleControlValue.value);
+  if (scaleValue === 100) {
+    scaleControlBigger.disabled = true;
+    return;
+  }
+  if (scaleValue >= 25) {
+    number = scaleValue + 25;
+    scaleControlSmaller.disabled = false;
+  }
+  if (scaleValue >= 75) {
+    scaleControlBigger.disabled = true;
+  }
+  scaleControlValue.value = number;
+  imgUploadPreview.style.transform = `scale(${number / 100})`;
+});
+
+// =========================================== Наложение эффекта ===============================================//
+let effectsRadio = document.querySelectorAll(".effects__radio");
+let imgUploadPreviewImg = imgUploadPreview.querySelector("img");
+effectsRadio.forEach((item) => {
+  item.addEventListener("change", (evt) => {
+    imgUploadPreviewImg.classList.value = ""; // При добавление нового класса, старый удаляется //
+    imgUploadPreviewImg.classList.add(`effects__preview--${evt.target.value}`);
+    if (imgUploadPreviewImg.classList.contains("effects__preview--none")) {
+      imgUploadEffectLevel.classList.add("hidden");
+    } else {
+      imgUploadEffectLevel.classList.remove("hidden");
+    }
+  });
+});
+
+// ===================================== Ползунок ===================================== //
+effectLevelPin.addEventListener("mouseup", () => {});
+// ===================================== Ползунок ===================================== //
 
 // ===================================== Прячем окно рндактирования фотографии ===================================== //
 uploadCancel.addEventListener("click", function () {
   imgUploadOverlay.classList.add("hidden");
   body.classList.remove("modal-open");
-  uploadFile.value = uploadFile.defaultValue; // Сбрасываем значение defaultValue //
+  uploadFile.value = uploadFile.defaultValue; // Сбрасываем значение окна загрузки файла //
+  scaleControlValue.value = "100"; // Возвращает значение поля увеличения //
+  imgUploadPreview.style.transform = `scale(${scaleControlValue.value / 100})`;
+  imgUploadPreviewImg.classList.value = "";
 });
 
-document.addEventListener("keydown", function (evt) {
+document.addEventListener("keydown", (evt) => {
   if (evt.key === "Escape") {
     imgUploadOverlay.classList.add("hidden");
     body.classList.remove("modal-open");
     uploadFile.value = uploadFile.defaultValue; // Сбрасываем значение defaultValue //
+    scaleControlValue.value = "100"; // Возвращает значение поля увеличения //
+    imgUploadPreview.style.transform = `scale(${
+      scaleControlValue.value / 100
+    })`;
+    imgUploadPreviewImg.classList.value = "";
   }
 });
 // ===================================== Прячем окно рндактирования фотографии ===================================== //
 
-// console.log(bigPictureCancel);
-// userDialog.querySelector('.setup-similar').classList.remove('hidden');
+// ===================================== Работа с # ===================================== //
+let textHashtags = document.querySelector(".text__hashtags");
+let minNameLength = 2;
+let maxNameLength = 20;
+let maxItemHashtags = 5;
+let textHashtagsValue = [];
+let space = " ";
+let comma = ",";
+textHashtags.addEventListener("input", () => {
+  textHashtagsValue = textHashtags.value;
+  onSplitString(textHashtagsValue, space);
+  let re = /^#[\w+]*$/g;
+  console.log(re.test(textHashtagsValue));
+  console.log(textHashtagsValue);
+  let valueLength = textHashtags.value.length;
+  textHashtagsValue.forEach((item) => {
+    // Провести сравнение на первый символ каждого элемента массива
+    // Провести сравнение на второй символ каждого элемента массива
+    // Сравнение на одинаковые элементы массива
 
-// radios.forEach((item) => {
-//   item.onchange = function (event) {
-//     if (this.dataset.evaluation !== "good") {
-//       // Сравинвает атрибут
-//       submitButton.disabled = true;
-//       error.classList.add("shown");
-//     } else {
-//       error.classList.remove("shown");
-//       submitButton.disabled = false;
-//       submitButton.addEventListener("click", function (evt) {
-//         evt.preventDefault();
-//       });
-//     }
-//   };
-// });
+    if (valueLength < minNameLength) {
+      textHashtags.setCustomValidity(
+        "Ещё " + (minNameLength - valueLength) + " симв."
+      ); // Не более 20 символов в элементе массива
+    } else if (item.length > maxNameLength) {
+      textHashtags.setCustomValidity(
+        "Удалите лишние " + (item.length - maxNameLength) + " симв."
+      ); // Не более 5 элементов массива
+    } else if (textHashtagsValue.length > maxItemHashtags) {
+      textHashtags.setCustomValidity(
+        "Удалите лишние " +
+          (textHashtagsValue.length - maxItemHashtags) +
+          " хештега."
+      );
+    } else {
+      textHashtags.setCustomValidity("");
+    }
+    textHashtags.reportValidity();
+  });
+});
+
+const onSplitString = (stringToSplit, space) => {
+  let splitStringText = stringToSplit.split(space);
+  textHashtagsValue = splitStringText.filter(Boolean); // Удалить пустой элемент массива
+};
+
+// textHashtagsValue.reduce((x, y) => (x.includes(y) ? x : [...x, y]), []);  Метод сортировки одинаковых элементов массива
+
+//let onTextHashtags = () => {};
+// textHashtags.addEventListener("invalid", () => {
+//    if (textHashtags.validity.tooShort) {
+//      textHashtags.setCustomValidity("Имя должно состоять минимум из 2-х символов");
+//    } else if (textHashtags.validity.tooLong) {
+//      textHashtags.setCustomValidity("Имя не должно превышать 20-ти символов");
+//       максимальная длина одного хэш-тега 20 символов
+//    } else {
+//      textHashtags.setCustomValidity("");
+//    }
+//  });
+//  console.log(textHashtags.value);
+
+// ===================================== Работа с # ===================================== //
